e875563cdeb11c286790b9b224df1996da274de3
Joe Barnes <barnesjd@gmail.com>
2015-08-22 17:04:38 -0500

    Maybe we can force unix file endings... still not convinced this is the problem, tho

--- apply.sh
+++ apply.sh
@@ -18,6 +18,8 @@ wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.3_linux_amd64.zip
 unzip terraform_0.6.3_linux_amd64.zip
 cd ..
 
+dos2unix *.tf
+
 tf=./terraform/terraform
 
 configureRemoteState () {
